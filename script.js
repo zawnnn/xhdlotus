7ed724740ed62746f901ff6f48810963150f0ed2
zawnnn <73693140+zawnnn@users.noreply.github.com>
2024-09-20 13:46:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Tạo một vùng tạm thời để sao chép
         const tempTextArea = document.createElement('textarea');
-        tempTextArea.value = `${taxNumber}\n${name}\n${address}\n${nhname}\n${note}`;
+        tempTextArea.value = `Mã số thuế: ${taxNumber}\nTên đơn vị: ${name}\n Địa chỉ: ${address}\nTên người mua hàng: ${nhname}\nGhi chú: ${note}`;
         document.body.appendChild(tempTextArea);
         tempTextArea.select();
         document.execCommand('copy');
